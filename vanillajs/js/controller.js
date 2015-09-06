61332f5c3b0691bece67087ca2fcad0385fdef7b
Xiaomin <xiaominhoo@163.com>
2015-09-06 22:06:15 +0800

    story1 & story2

---
+++ vanillajs/js/controller.js
@@ -0,0 +1,75 @@
+(function (window) {
+	'use strict';
+
+	/**
+	 * Takes a model and view and acts as the controller between them
+	 *
+	 * @constructor
+	 * @param {object} model The model instance
+	 * @param {object} view The view instance
+	 */
+	function Controller(model, view) {
+		var self = this;
+		self.model = model;
+		self.view = view;
+
+		self.view.bind('newTodo', function (title) {
+			self.addItem(title);
+		});
+
+	}
+
+
+	/**
+	 * An event to fire whenever you want to add an item. Simply pass in the event
+	 * object and it'll handle the DOM insertion and saving of the new item.
+	 */
+	Controller.prototype.addItem = function (title) {
+		var self = this;
+
+		if (title.trim() === '') {
+			return;
+		}
+
+		self.model.create(title, function () {
+			self.view.render('clearNewTodo');
+			self._filter(true);
+		});
+	};
+
+	/**
+	 * Re-filters the todo items, based on the active route.
+	 * @param {boolean|undefined} force  forces a re-painting of todo items.
+	 */
+	Controller.prototype._filter = function (force) {
+//		var activeRoute = this._activeRoute.charAt(0).toUpperCase() + this._activeRoute.substr(1);
+
+		// Update the elements on the page, which change with each completed todo
+	//	 this._updateCount();
+
+		// If the last active route isn't "All", or we're switching routes, we
+		// re-create the todo item elements, calling:
+		//   this.show[All|Active|Completed]();
+		if (force || this._lastActiveRoute !== 'All' || this._lastActiveRoute !== activeRoute) {
+			this['showAll']();
+		}
+
+//		this._lastActiveRoute = activeRoute;
+	};
+
+
+	/**
+	 * An event to fire on load. Will get all items and display them in the
+	 * todo-list
+	 */
+	Controller.prototype.showAll = function () {
+		var self = this;
+		self.model.read(function (data) {
+			self.view.render('showEntries', data);
+		});
+	};
+
+	// Export to window
+	window.app = window.app || {};
+	window.app.Controller = Controller;
+})(window);
