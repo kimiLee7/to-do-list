293f4970db785636743f8e6e4340118f1cb14856
Xiaomin <xiaominhoo@163.com>
2015-09-12 20:37:36 +0800

    complete story3

---
+++ vanillajs/view.js
@@ -0,0 +1,73 @@
+/*global qs, qsa, $on, $parent, $delegate */
+
+(function (window) {
+	'use strict';
+
+	/**
+	 * View that abstracts away the browser's DOM completely.
+	 * It has two simple entry points:
+	 *
+	 *   - bind(eventName, handler)
+	 *     Takes a todo application event and registers the handler
+	 *   - render(command, parameterObject)
+	 *     Renders the given command with the options
+	 */
+	function View(template) {
+		this.template = template;
+//		this.ENTER_KEY = 13;
+//		this.ESCAPE_KEY = 27;
+		this.$todoList = qs('.todo-list');
+//		this.$main = qs('.main');
+		this.$newTodo = qs('.new-todo');
+	}
+
+	View.prototype._removeItem = function (id) {
+		var elem = qs('[data-id="' + id + '"]');
+
+		if (elem) {
+			this.$todoList.removeChild(elem);
+		}
+	};
+
+
+	View.prototype.render = function (viewCmd, parameter) {
+		var self = this;
+		var viewCommands = {
+			showEntries: function () {
+				self.$todoList.innerHTML = self.template.show(parameter);
+			},
+			clearNewTodo: function () {
+				self.$newTodo.value = '';
+			},
+			removeItem: function () {
+				self._removeItem(parameter);
+			}
+		};
+
+		viewCommands[viewCmd]();
+	};
+
+	View.prototype._itemId = function (element) {
+		var li = $parent(element, 'li');
+		return parseInt(li.dataset.id, 10);
+	};
+
+	View.prototype.bind = function (event, handler) {
+		var self = this;
+		if (event === 'newTodo') {
+			$on(self.$newTodo, 'change', function () {
+				handler(self.$newTodo.value);
+			});
+
+		}else if (event === 'itemRemove') {
+			$delegate(self.$todoList, '.destroy', 'click', function () {
+					handler({id: self._itemId(this)});
+			});
+
+		}
+	};
+
+	// Export to window
+	window.app = window.app || {};
+	window.app.View = View;
+}(window));
